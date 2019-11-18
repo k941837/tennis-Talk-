d828704fc875a766cbd64689ea134d924c1f35fb
k941837 <55436678+k941837@users.noreply.github.com>
2019-11-18 15:36:47 +0900

    tennis talk

--- app/controllers/tweets/searches_controller.rb
+++ app/controllers/tweets/searches_controller.rb
@@ -1,5 +1,6 @@
 class Tweets::SearchesController < ApplicationController
   def index
-    @tweets = Tweet.search(params[:keyword])
+    # @tweets = Tweet.search(params[:keyword])
+    @tweets = Tweet.where('racket LIKE ?', "%#{params[:keyword]}%")
   end
 end
